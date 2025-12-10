703a86c95478e3b6242b752d0a58f8a17577335a
anabotr <ana.botr@gmail.com>
2025-12-10 10:26:31 -0300

    Finalizada Q1 simulado MatWerner

--- RevisaoA2/Q1.py
+++ RevisaoA2/Q1.py
@@ -3,6 +3,7 @@ from abc import ABC, abstractmethod
 class FitSquareError(Exception): ...
 class PlanError(FitSquareError): ...
 class ExtraNotFoundError(PlanError): ...
+class FeatureNotFoundError(PlanError): ...
 
 class Plan(ABC):
     available_extras = {
@@ -46,20 +47,25 @@ class Plan(ABC):
         
     def plan_type(self):
         return self._plan_type
+    
+    @classmethod
+    def add_feature(cls, feature):
+        cls.stand_features = cls.stand_features + feature
 
 class PremiumPlan(Plan):
+    stand_features = ["Personal"]
     def __init__(self, base_price):
         self._plan_type = "Premium"  # "basic" ou "premium"
         self._price = base_price
-        self._extras = []            # lista de strings
-    
+        self._extras = [] 
 
         
 class BasicPlan(Plan):
+    stand_features = []
     def __init__(self, base_price):
         self._plan_type = "Basic"  # "basic" ou "premium"
         self._price = base_price
-        self._extras = []            # lista de strings
+        self._extras = []
 
 
 class Member:
@@ -69,6 +75,9 @@ class Member:
         self._plan = plan
         self._features = ["aulas em grupo"]
     
+    def name(self):
+        return self._name
+
     @property
     def plan(self):
         return self._plan
@@ -78,42 +87,39 @@ class Member:
         self._plan = plan
 
     def features(self):
-        return self._features
+        return self._features + self.plan.__class__.stand_features
 
     def rm_features(self, features):
         if isinstance(features, list):
             for feature in features:
-                self.features.remove(feature)
+                if feature in self._features:
+                    self._features.remove(feature)
+                raise ExtraNotFoundError("This extra doesn't exist")
+        elif features in self._features:
+            self._features.remove(features)
         else:
-            self.features.remove(features)
+            raise ExtraNotFoundError("This extra doesn't exist")
 
     def add_features(self, features):
         if isinstance(features, list):
             for feature in features:
-                self.features.append(feature)
+                self._features.append(feature)
         else:
-            self.features.append(features)
+            self._features.append(features)
 
 
     def upgrade_features(self):
-        if self.plan.plan_type() == "Premium":
-            self.add_features("Personal")
-        else:
-            if "Personal" in self.features:
-                self.rm_features("Personal")
-        for extra in self.plan.extras:
-            self.add_features(extra)
+        self.add_features(self.plan.extras)
 
 
     def upgrade_plan(self, new_plan : BasicPlan | PremiumPlan):
         old_price = self.plan.get_total_price()
         new_price = new_plan.get_total_price()
         diff = new_price - old_price
+        string = f"{self.name()} está mudando de {self.plan.plan_type()} para {new_plan.plan_type()}.\nPreço antigo: R$ {old_price}, novo preço: R$ {new_price}. Diferença: R$ {diff}."
         self.plan = new_plan
         self.upgrade_features()
-
-        return f"{self.name} está mudando de {self.plan.plan_type()} para {new_plan.plan_type()}.\nPreço antigo: R$ {old_price}, novo preço: R$ {new_price}. Diferença: R$ {diff}."
-
+        return string
 
     def describe_features(self):
         return self._features
@@ -131,3 +137,26 @@ class SubscriptionService:
         member.upgrade_plan(new_plan)
 
         print("Upgrade concluído!")
+
+
+# Não mexi no SubscriptionService mas ele poderia ter mais funções (quebra o SRP)
+
+#Driver Code
+
+plano1 = BasicPlan(100)
+plano2 = PremiumPlan(200)
+ana = Member("ana", "ana@gmail", plano1)
+print(plano1.available_extras)
+print(plano1.extras)
+#print(plano1.add_extras("nutri")) quebra o programa
+print(plano1.add_extras("nutricao"))
+print(plano1.extras)
+
+print(ana.features())
+print(ana.add_features("bike"))
+print(ana.features())
+#print(ana.rm_features("caminhada")) quebra o programa aqui
+print(ana.describe_features())
+print(ana.rm_features("bike"))
+print(ana.describe_features())
+print(ana.upgrade_plan(plano2))
